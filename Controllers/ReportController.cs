93d77eebd3480ef4d233412648bb3e094bd1a4eb
jeftegoesdev <jefte.goes.dev@outlook.com>
2020-11-02 16:40:32 -0300

    Commit

---
+++ Controllers/ReportController.cs
@@ -0,0 +1,36 @@
+using System.IO;
+using FastReport;
+using FastReport.Export.Pdf;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProjetoExternalNugetRefVsCode.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ReportController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult GetReport()
+        {
+            // Create report instance.
+            Report report = new Report();
+
+            // Load the existing report.
+            report.Load("Report/Test.frx");
+
+            // Prepare the report.
+            report.Prepare();
+
+            // Create export instance
+            PDFExport export = new PDFExport();
+
+            // Transform result and return to client.
+            using (MemoryStream ms = new MemoryStream())
+            {
+                report.Export(export, ms);
+                ms.Flush();
+                return File(ms.ToArray(), "application/pdf", Path.GetFileNameWithoutExtension("Test") + ".pdf");
+            }
+        }
+    }
+}
